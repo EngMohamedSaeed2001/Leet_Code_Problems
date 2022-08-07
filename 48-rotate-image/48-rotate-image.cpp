81e0fa60d06bcd1fc607f7c31395edc862a9a6f6
Mohamed Saeed <74047939+EngMohamedSaeed2001@users.noreply.github.com>
2022-08-07 18:16:38 +0200

    Time: 6 ms (23.31%), Space: 7.1 MB (33.91%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int size=matrix.at(0).size();
-        unordered_map<int,vector<int>>map;
-        for (int k=size-1; k>=0 ; k--) {
-            for (int i =size-1; i >= 0; i--) {
-                map[k].push_back(matrix.at(i).at(k));
-            }
-        }
-        for (int i = 0; i < size; ++i) {
-            matrix.at(i).swap(map[i]);
-        }
+   void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
     }
+}
 };
