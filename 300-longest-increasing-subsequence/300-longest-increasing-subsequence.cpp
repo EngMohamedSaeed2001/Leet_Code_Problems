f6ba50d9885180b8665d9d77d757d32356021cf7
Mohamed Saeed <74047939+EngMohamedSaeed2001@users.noreply.github.com>
2022-08-08 16:36:16 +0200

    Time: 1006 ms (15.63%), Space: 10.5 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
    int lengthOfLIS(vector<int>& nums) {
         int max=0;
-        vector<int>index;
-        for (int i = 0; i < nums.size(); ++i) {
-            index.push_back(1);
-        }
+        vector<int>index(nums.size(),1);
 
-        for (int i = 1; i < nums.size(); ++i) {
+        for (int i = 1; i <nums.size() ; ++i) {
             for (int j = 0; j < i; ++j) {
-                if(nums.at(i)>nums.at(j)&&index.at(j)+1>index.at(i)){
+                if(nums.at(i)>nums.at(j)&&index.at(j)+1>index.at(i))
                     index.at(i)=index.at(j)+1;
-                }
             }
         }
 
-        sort(index.begin(),index.end());
-
-        max=index.at(index.size()-1);
-
+        auto n=std::max_element(index.begin(), index.end());
+        max=*n;
         return max;
     }
 };
